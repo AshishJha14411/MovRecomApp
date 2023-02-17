36d4f11c46555b1ac69ed86cf09615bd1b26c516
Ashish Jha <ashishjha14411@gmail.com>
2023-02-17 22:41:13 +0530

    added type for MovieLists

--- src/context/types.ts
+++ src/context/types.ts
@@ -11,3 +11,24 @@ export interface GenreContext {
   genreId: number | null;
   setGenreId: Dispatch<SetStateAction<number>>;
 }
+export type MovieList = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: [number];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: Date;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+export interface MovieContext {
+  movieList: MovieList[];
+  movieId: number | null;
+  setMovieId: Dispatch<SetStateAction<number>>;
+}
