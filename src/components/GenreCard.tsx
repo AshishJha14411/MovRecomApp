eb1e5b9aa9608e1c6add3bb1b5df2f1727c92c56
Ashish Jha <ashishjha14411@gmail.com>
2023-02-17 22:22:16 +0530

    added genre components

--- src/components/GenreCard.tsx
+++ src/components/GenreCard.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
-
+import { useContext } from 'react'
+import { GenresContext } from '../context/GenreContext'
+import { GenreContext } from '../context/types';
 const GenreCard = () => {
+  const { genres, setGenreId } = useContext(GenresContext) as GenreContext
+
+
+
   return (
-    <div>GenreCard</div>
+    <div className='w-[80%] flex flex-row flex-wrap mx-auto'>
+      {genres && genres.map((item) => {
+        return (
+          <div className='w-[20%]' key={item.id}>
+
+            <button type="button" onClick={() => setGenreId(item.id)} className="px-[3rem] mx-[1rem] my-[1rem] py-[1.5rem] font-semibold rounded-full bg-gray-800 text-gray-100">{item.name}</button>
+          </div>
+
+        )
+      })}
+    </div>
   )
 }
 
