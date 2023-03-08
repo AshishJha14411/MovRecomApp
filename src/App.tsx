201cf0ba5960efc34980495bfdf555bf75596aec
Ashish Jha <ashishjha14411@gmail.com>
2023-03-08 19:45:56 +0530

    added the router and context at highest parent

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,48 @@
+
 import './App.css';
+import axios from 'axios';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GenreCard from './components/GenreCard';
+import React, { useContext, useEffect } from 'react'
+
 import Home from './components/Home';
-import { GenreListProvider } from './context/GenreContext';
-import { MovieListProvider } from './context/MovieContext';
 import { VideoProvider } from './context/VideoContext';
 
-function App() {
+import { GenreContext, MovieContext, VideoContext } from './context/types';
+import { VideosContext } from './context/VideoContext'
+import MovieCard from './components/MovieCard';
+import { MovieSearchProvider } from './context/MovieSearch';
+import Navbar from './components/Navbar';
+import { GenresContext } from './context/GenreContext';
+import TrendingPage from './components/TrendingPage';
+import MoviePage from './components/MoviePage';
+import { MoviesContext } from './context/MovieContext';
 
+import { useNavigate } from 'react-router-dom'
+function App() {
+  
+  const {movieList,singleMovie, setSingleMovie,movieId, setMovieId} = useContext(MoviesContext) as MovieContext
+  const {genreId, setGenreId} = useContext(GenresContext) as GenreContext
+ 
   return (
-    <GenreListProvider>
-      <MovieListProvider>
-      <VideoProvider>
-        <Home />
-      </VideoProvider>
-      </MovieListProvider>
-    </GenreListProvider>
-    );
+    <div className=''>
+    <VideoProvider>
+      <MovieSearchProvider>
+      
+        <Navbar />
+          <Routes>
+              <Route path="/" element= {<Home />} />
+              <Route path="genrecard" element={genreId ? <MovieCard /> : <GenreCard />} />
+              <Route path="moviecard" element={<MovieCard />} />
+              <Route path="trending" element={<TrendingPage />} />
+              <Route path="singleMovie" element={<MoviePage />} />
+          </Routes>
+       
+      </MovieSearchProvider>
+    </VideoProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { GenreListProvider, GenresContext } from './context/GenreContext';
+import { MovieListProvider } from './context/MovieContext';
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <>
-    <App />
-  </>
+  <BrowserRouter>
+    <GenreListProvider>
+      <MovieListProvider>
+        <App />
+      </MovieListProvider>
+    </GenreListProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
