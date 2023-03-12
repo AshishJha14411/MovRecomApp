4c4be2a93e1d33353064ab88e0f23cf82a637f6f
Ashish Jha <ashishjha14411@gmail.com>
2023-03-12 16:09:24 +0530

    changed colors in GenreCard

--- src/components/GenreCard.tsx
+++ src/components/GenreCard.tsx
@@ -1,20 +1,19 @@
-import { useContext } from 'react'
+import  { useContext } from 'react'
 import { GenresContext } from '../context/GenreContext'
 import { GenreContext } from '../context/types';
 const GenreCard = () => {
-  /* Importing the Genre Context */
   const { genres, setGenreId } = useContext(GenresContext) as GenreContext
   return (
     <div className='w-[80%] flex flex-row flex-wrap mx-auto'>
-      <h1 className='w-[100%] pl-[2rem] pr-[40%] pb-8 text-[#8D021F] font-semibold text-4xl '>Unleash the thrill of cinema: Explore and Discover trailers from your favorite genres with Genre Finder.</h1>
       {genres && genres.map((item) => {
         return (
           <div className='w-[20%]' key={item.id}>
-            <button type="button" onClick={() => setGenreId(item.id)} className="px-[3rem] mx-[1rem] text-[#EC3D37] my-[1rem] py-[1.5rem] font-semibold rounded-full bg-gray-800">{item.name}</button>
+            <button type="button" onClick={() => setGenreId(item.id)} className="px-[3rem] mx-[1rem] text-[#B5F1FA] my-[1rem] py-[1.5rem] font-semibold rounded-full bg-[#0e2a51] hover:bg-[#0254AF]  transition ease-in-out duration-700">{item.name}</button>
           </div>
         )
       })}
     </div>
+
   )
 }
 
